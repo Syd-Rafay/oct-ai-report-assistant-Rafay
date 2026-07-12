9c427d413c1364d4639970db6f74828fd223a0ba
Raahym <raahymm@gmail.com>
2026-07-12 17:45:28 +0500

    Remove dashboard page component

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,1 @@
-import { AppShell } from "@/components/app-shell";
-import { DashboardView } from "@/components/views";
 
-export default function Page() {
-  return (
-    <AppShell>
-      <DashboardView />
-    </AppShell>
-  );
-}
